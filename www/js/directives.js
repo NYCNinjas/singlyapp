c4ef11fe5e08129a09c37f58b5ae12aaff355e35
Maryia Piletskaya <hey.november@gmail.com>
2016-01-11 16:05:46 -0500

    added check for the passwords to match

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,6 +1,27 @@
 angular.module('app.directives', [])
 
-.directive('blankDirective', [function(){
+.directive('equalsTo', [function () {
+    /*
+     * <input type="password" ng-model="Password" />
+     * <input type="password" ng-model="ConfirmPassword" equals-to="Password" />
+     */
+    return {
+        restrict: 'A', // Use only as attribute
+        scope: true,
+        require: 'ngModel',
+        link: function (scope, elem, attrs, control) {
+            var check = function () {
+                var v1 = scope.$eval(attrs.ngModel); // attrs.ngModel = "ConfirmPassword"                   
+                var v2 = scope.$eval(attrs.equalsTo).$viewValue; // attrs.equalsTo = "Password"
+                return v1 == v2;
+            };
+            scope.$watch(check, function (isValid) {
+                control.$setValidity("equalsTo", isValid);
+            });
+        }
+    };
+
+// .directive('blankDirective', [function(){
 
 }]);
 
