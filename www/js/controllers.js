c0ccc03626b8cd9a91ac8f700c6a1bc7b19f6131
Maryia Piletskaya <hey.november@gmail.com>
2016-01-11 14:20:28 -0500

    added signup functionality without all error handling

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,33 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('signupCtrl', function($scope) {
+.controller('signupCtrl', function($scope, $state) {
+	$scope.user = {};
 
+ 	$scope.signupEmail = function(){  
+ 	  //Create a new user on Parse
+
+ 	  //TODO check that confirmation password match!
+	  var user = new Parse.User();
+	  user.set("username", $scope.user.username);
+	  user.set("password", $scope.user.password);
+	  user.set("email", $scope.user.email);
+	 
+	  user.signUp(null, {
+	    success: function(user) {
+	      // Hooray! Let them use the app now.
+	      alert("success!");
+	    },
+	    error: function(user, error) {
+	      // Show the error message somewhere and let the user try again.
+	      alert("Error: " + error.code + " " + error.message);
+	    }
+	  });
+	};
+ 
+  	$scope.loginEmail = function(){
+ 
+  	};
 })
    
 .controller('mainCtrl', function($scope) {
